4ec3658442c52b85d5a40afd9b9bc3c9783955e8
akorovai <aktey.main@gmail.com>
2024-02-02 02:09:38 +0100

    project initialization

---
+++ src/main/java/dev/akorovai/AdvancedToDoAPI/AdvancedToDoApiApplication.java
@@ -0,0 +1,13 @@
+package dev.akorovai.AdvancedToDoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedToDoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedToDoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/akorovai/AdvancedToDoAPI/AdvancedToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.akorovai.AdvancedToDoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedToDoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
