498a0123e9ba19beeeaad4e93666783cd9ca141c
akorovai <aktey.main@gmail.com>
2024-06-02 14:46:59 +0200

    feat: modify TaskHistoryServiceImpl.java

--- src/main/java/dev/akorovai/AdvancedToDoAPI/service/TaskHistoryServiceImpl.java
+++ src/main/java/dev/akorovai/AdvancedToDoAPI/service/TaskHistoryServiceImpl.java
@@ -3,11 +3,9 @@ package dev.akorovai.AdvancedToDoAPI.service;
 import dev.akorovai.AdvancedToDoAPI.dto.TaskHistoryDto;
 import dev.akorovai.AdvancedToDoAPI.entity.TaskHistory;
 import dev.akorovai.AdvancedToDoAPI.repository.TaskHistoryRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional
-@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@RequiredArgsConstructor
 @Slf4j
 public class TaskHistoryServiceImpl implements TaskHistoryService {
 
@@ -28,22 +25,25 @@ public class TaskHistoryServiceImpl implements TaskHistoryService {
     public List<TaskHistoryDto> getTaskHistories(Long taskId, LocalDateTime from, LocalDateTime to, String sortBy) {
         log.debug("Fetching task histories with taskId: {}, from: {}, to: {}, sortBy: {}", taskId, from, to, sortBy);
 
-        List<TaskHistory> taskHistories;
-        if (taskId != null && from != null && to != null) {
-            taskHistories = taskHistoryRepository.findByTaskIdAndTimestampBetween(taskId, from, to, Sort.by(Sort.Direction.ASC, sortBy));
-        } else if (taskId != null) {
-            taskHistories = taskHistoryRepository.findByTaskId(taskId, Sort.by(Sort.Direction.ASC, sortBy));
-        } else if (from != null && to != null) {
-            taskHistories = taskHistoryRepository.findByTimestampBetween(from, to, Sort.by(Sort.Direction.ASC, sortBy));
-        } else {
-            taskHistories = taskHistoryRepository.findAll(Sort.by(Sort.Direction.ASC, sortBy));
-        }
+        List<TaskHistory> taskHistories = getTaskHistoriesFromRepository(taskId, from, to, sortBy);
 
         log.debug("Found {} task histories", taskHistories.size());
 
         return taskHistories.stream()
                 .map(this::convertToDto)
                 .collect(Collectors.toList());
+    }
+
+    private List<TaskHistory> getTaskHistoriesFromRepository(Long taskId, LocalDateTime from, LocalDateTime to, String sortBy) {
+        if (taskId != null && from != null && to != null) {
+            return taskHistoryRepository.findByTaskIdAndTimestampBetween(taskId, from, to, Sort.by(Sort.Direction.ASC, sortBy));
+        } else if (taskId != null) {
+            return taskHistoryRepository.findByTaskId(taskId, Sort.by(Sort.Direction.ASC, sortBy));
+        } else if (from != null && to != null) {
+            return taskHistoryRepository.findByTimestampBetween(from, to, Sort.by(Sort.Direction.ASC, sortBy));
+        } else {
+            return taskHistoryRepository.findAll(Sort.by(Sort.Direction.ASC, sortBy));
+        }
     }
 
     private TaskHistoryDto convertToDto(TaskHistory taskHistory) {
